c098182b4b97fbff59113dd0865e381fa4533517
Valentyn Boichuk <bvalentin1214@gmail.com>
2024-09-29 21:49:47 +0300

    Polish-up service

--- app/src/main/java/com/kitahara/scamdtek/presentation/overlay/OverlayService.kt
+++ app/src/main/java/com/kitahara/scamdtek/presentation/overlay/OverlayService.kt
@@ -15,17 +15,14 @@ import android.view.View
 import android.view.WindowManager
 import android.view.animation.AccelerateDecelerateInterpolator
 import com.kitahara.scamdtek.R
-import com.kitahara.scamdtek.common.logDebug
 import com.kitahara.scamdtek.common.toast
 import com.kitahara.scamdtek.presentation.contact_detail.ContactDetailActivity.Companion.launchContactDetailActivity
 
-/**
- * Copied from [repository](https://github.com/noln/system-alert-window-example/blob/master/app/src/main/java/com/mattfenlon/ghost/MainService.java) with some adjustments
- */
 class OverlayService : Service() {
     private var windowManager: WindowManager? = null
     private var floatyView: View? = null
     private var contactNumber: String? = null
+    private lateinit var params: WindowManager.LayoutParams
 
     override fun onBind(intent: Intent): IBinder? {
         return null
@@ -49,7 +46,7 @@ class OverlayService : Service() {
         } else {
             WindowManager.LayoutParams.TYPE_PHONE
         }
-        val params = WindowManager.LayoutParams(
+        params = WindowManager.LayoutParams(
             WindowManager.LayoutParams.WRAP_CONTENT,
             WindowManager.LayoutParams.WRAP_CONTENT,
             layoutParamsType,
@@ -58,85 +55,7 @@ class OverlayService : Service() {
         ).apply {
             gravity = Gravity.CENTER or Gravity.END
         }
-        val inflater = getSystemService(LayoutInflater::class.java)
-        floatyView = inflater.inflate(R.layout.floating_view, null)
-
-        floatyView?.setOnTouchListener(object : View.OnTouchListener {
-            private var initialX = 0
-            private var initialY = 0
-            private var initialTouchX = 0f
-            private var initialTouchY = 0f
-
-            override fun onTouch(v: View, event: MotionEvent): Boolean {
-                logDebug("Action triggered: ${event.action}")
-                when (event.action) {
-                    // Called when moving action has started
-                    MotionEvent.ACTION_DOWN -> {
-                        // Remember initial position and touch point
-                        initialX = params.x
-                        initialY = params.y
-                        initialTouchX = event.rawX
-                        initialTouchY = event.rawY
-                        return true
-                    }
-
-                    MotionEvent.ACTION_MOVE -> {
-                        // Calculate new X and Y coordinates of the view
-                        params.x = initialX + (initialTouchX - event.rawX).toInt()
-                        params.y = initialY + (event.rawY - initialTouchY).toInt()
-
-                        // Update the layout with the new X and Y coordinates
-                        windowManager?.updateViewLayout(floatyView, params)
-                        return true
-                    }
-
-                    // Called when pressed state is ended
-                    MotionEvent.ACTION_UP -> {
-                        if (initialX == params.x && initialY == params.y) {
-                            // Perform activity launch when no changes in coordinates were observed
-                            launchDetailActivity()
-                        } else {
-                            // Move overlay to start or end by X if it was triggered
-                            val metrics = if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.R) {
-                                windowManager?.currentWindowMetrics
-                            } else {
-                                TODO("VERSION.SDK_INT < R")
-                            }
-
-                            val screenWidth = metrics?.bounds?.width()
-                            val finalXCoordinate = if (screenWidth == null || params.x  >= (screenWidth/2) ) {
-                                screenWidth ?: 0
-                            } else {
-                                0
-                            }
-                            // Animate position by updating the params.x directly
-                            val startX = params.x
-                            ValueAnimator.ofInt(startX, finalXCoordinate).apply {
-                                duration = 300
-                                interpolator = AccelerateDecelerateInterpolator()
-                                addUpdateListener { animation ->
-                                    params.x = animation.animatedValue as Int
-                                    windowManager?.updateViewLayout(floatyView, params)
-                                }
-                                start()
-                            }
-                        }
-                        return true
-                    }
-                }
-                return false
-            }
-        })
-        windowManager?.addView(floatyView, params)
-    }
-
-    private fun launchDetailActivity() {
-        if (contactNumber != null) {
-            launchContactDetailActivity(contactNumber!!)
-        } else {
-            toast("Contact number cannot be null")
-            onDestroy() // Finish service
-        }
+        setupView()
     }
 
     override fun onDestroy() {
@@ -147,8 +66,88 @@ class OverlayService : Service() {
         }
     }
 
+    @SuppressLint("ClickableViewAccessibility")
+    private fun setupView() {
+        var initialX = 0
+        var initialY = 0
+        var initialTouchX = 0f
+        var initialTouchY = 0f
+
+        val inflater = getSystemService(LayoutInflater::class.java)
+        floatyView = inflater.inflate(R.layout.floating_view, null)
+        floatyView?.setOnTouchListener { _, event ->
+            when (event.action) {
+                // Called when moving action has started
+                MotionEvent.ACTION_DOWN -> {
+                    // Remember initial position and touch point
+                    initialX = params.x
+                    initialY = params.y
+                    initialTouchX = event.rawX
+                    initialTouchY = event.rawY
+                    return@setOnTouchListener true
+                }
+
+                MotionEvent.ACTION_MOVE -> {
+                    // Calculate new X and Y coordinates of the view
+                    params.x = initialX + (initialTouchX - event.rawX).toInt()
+                    params.y = initialY + (event.rawY - initialTouchY).toInt()
+                    // Update the layout with the new X and Y coordinates
+                    windowManager?.updateViewLayout(floatyView, params)
+                    return@setOnTouchListener true
+                }
+
+                // Called when pressed state is ended
+                MotionEvent.ACTION_UP -> {
+                    if (initialX == params.x && initialY == params.y) {
+                        launchDetailActivity()
+                    } else moveOverlayToEdge()
+                    return@setOnTouchListener true
+                }
+            }
+            false
+        }
+        windowManager?.addView(floatyView, params)
+    }
+
+    private fun moveOverlayToEdge() {
+        // Move overlay to start or end by X if it was triggered
+        val metrics = if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.R) {
+            windowManager?.currentWindowMetrics
+        } else {
+            TODO("VERSION.SDK_INT < R")
+        }
+
+        val screenWidth = metrics?.bounds?.width()
+        val finalXCoordinate = if (screenWidth == null || params.x >= (screenWidth / 2)) {
+            screenWidth ?: 0
+        } else {
+            0
+        }
+        // Animate position by updating the params.x directly
+        val startX = params.x
+        ValueAnimator.ofInt(startX, finalXCoordinate).apply {
+            duration = OVERLAY_TRANSITION_DURATION
+            interpolator = AccelerateDecelerateInterpolator()
+            addUpdateListener { animation ->
+                params.x = animation.animatedValue as Int
+                windowManager?.updateViewLayout(floatyView, params)
+            }
+            start()
+        }
+    }
+
+    private fun launchDetailActivity() {
+        if (contactNumber != null) {
+            launchContactDetailActivity(contactNumber!!)
+        } else {
+            toast("Contact number cannot be null")
+        }
+        onDestroy() // Finish service
+    }
+
     companion object {
         private const val EXTRA_PHONE_NUMBER = "PhoneNumber"
+        private const val OVERLAY_TRANSITION_DURATION = 300L
 
         fun Context.launchOverlayService(phoneNumber: String) {
             val overlayServiceIntent = Intent(this, OverlayService::class.java)
