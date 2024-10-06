9990a818e51f8832350327c4f2cf4d83eddbda65
Valentyn Boichuk <bvalentin1214@gmail.com>
2024-10-06 15:57:03 +0300

    Fix a whitespace

--- app/src/main/java/com/kitahara/scamdtek/presentation/incoming/IncomingCallService.kt
+++ app/src/main/java/com/kitahara/scamdtek/presentation/incoming/IncomingCallService.kt
@@ -6,6 +6,7 @@ import com.kitahara.scamdtek.common.logDebug
 import com.kitahara.scamdtek.presentation.overlay.OverlayService.Companion.launchOverlayService
 
 class IncomingCallService : CallScreeningService() {
+
     override fun onScreenCall(callDetails: Call.Details) {
         val phoneNumber = callDetails.handle.schemeSpecificPart
         logDebug("onScreenCall: $phoneNumber")
